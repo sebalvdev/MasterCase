7b68d10b59301db18fe9f6e8bd60b64a3a5d6c2d
sebalvdev <sebalvbus@gmail.com>
2024-11-11 09:15:26 -0400

    added page jugability

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:master_case/features/scanner_qr/presentation/pages/mobile_scanner_example.dart';
-import 'package:master_case/features/scanner_qr/presentation/pages/qr_scanner_page.dart';
-import 'package:master_case/features/scanner_qr/presentation/widgets/scanner_qr_widget.dart';
-import 'package:master_case/features/welcome_screen/presentation/pages/welcome_page.dart';
+import 'package:master_case/features/jugability/presentation/pages/jugability_page.dart';
 import 'package:master_case/firebase_options.dart';
 
 import 'config/routes/app_routes.dart';
 import 'config/themes/default_theme.dart';
 // import 'features/home/presentation/pages/home_page.dart';
-import 'features/menu/presentation/pages/menu_page.dart';
+// import 'features/menu/presentation/pages/menu_page.dart';
 import 'injection_container.dart' as dependencies;
 
 Future<void> main() async {
@@ -38,7 +35,9 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: AppRoutes.onGenerateRoutes,
       title: 'Master Case App',
       theme: defaultTheme(),
-      home: const WelcomePage(),
+      // home: const WelcomePage(),
+      // home: const MenuPage(),
+      home: const JugabilityPage(),
     );
   }
 }
